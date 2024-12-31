61f48f1770ba883f7f34cd12deaf30774600df53
Priscilla DigiTech <priscilladigitech@gmail.com>
2024-12-31 07:15:56 +0100

    31/12/24 07:15

---
+++ assets/js/index.js
@@ -0,0 +1,37 @@
+//OUVERTURE DU SOUS MENU
+
+//Sous menu 1
+
+const sousNav1 = document.getElementById("sousNav1");
+
+function openSousNav1() {
+  sousNav1.style.display = "block";
+}
+
+function closeSousNav1() {
+  sousNav1.style.display = "none";
+}
+
+//Sous menu 2
+
+const sousNav2 = document.getElementById("sousNav2");
+
+function openSousNav2() {
+  sousNav2.style.display = "block";
+}
+
+function closeSousNav2() {
+  sousNav2.style.display = "none";
+}
+
+//Sous menu 3
+
+const sousNav3 = document.getElementById("sousNav3");
+
+function openSousNav3() {
+  sousNav3.style.display = "block";
+}
+
+function closeSousNav3() {
+  sousNav3.style.display = "none";
+}
